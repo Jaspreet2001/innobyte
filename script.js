93212c81f3f3f40b230044da1d5f1bf672a26f2e
Jaspreet Kaur <78601370+Jaspreet2001@users.noreply.github.com>
2024-07-20 18:35:28 +0530

    add event listeners

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const galleryList = document.querySelector(".gallery-list");
+
+let currentIndex = 0;
+const itemsToShow = 2; // Number of images to show at a time
+
+function updateSlider() {
+  const itemWidth = galleryList.querySelector(".gallery-item").offsetWidth;
+  galleryList.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+}
+
+prevButton.addEventListener("click", () => {
+  const totalItems = galleryList.children.length;
+  currentIndex =
+    (currentIndex - 1 + totalItems / itemsToShow) % (totalItems / itemsToShow);
+  updateSlider();
+});
+
+nextButton.addEventListener("click", () => {
+  const totalItems = galleryList.children.length;
+  currentIndex = (currentIndex + 1) % (totalItems / itemsToShow);
+  updateSlider();
+});
+
+// Initialize the slider
+updateSlider();
+
+/**
+ * Navbar Toggle Event
+ */
+const navToggleEvent = function () {
+  window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    const goTopBtn = document.querySelector(".go-top-btn");
+
+    if (window.scrollY > 200) {
+      header.classList.add("active");
+      goTopBtn.classList.add("active");
+    } else {
+      header.classList.remove("active");
+      goTopBtn.classList.remove("active");
+    }
+  });
+};
+
+// Initialize Navbar Toggle
+navToggleEvent();
